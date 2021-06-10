5a31b56bbebca76e649d0138a43d4eebec68cbf3
calvin <289016698@qq.com>
2021-06-10 16:43:55 +0800

    permission

--- src/permission.js
+++ src/permission.js
@@ -2,24 +2,25 @@ import router from "@/router"
 import store from './store'
 
 
-router.beforeEach(async(to, from, next) => {
+router.beforeEach(async (to, from, next) => {
   console.log("beforeEach ENTER", from.path, "=>", to.path)
   let token = localStorage.getItem('token')
   const hasRoles = store.getters.roles && store.getters.roles.length > 0
-  if(to.path === '/login') {
-   next()
-  } else if(!token) {
+
+  if (to.path === '/login') {
+    next()
+  } else if (!token) {
     next('/login')
-  }else if(hasRoles) {
+  } else if (!hasRoles) {
+
+    let { data } = await store.dispatch('user/GetInfo', token)
+    const accessedRoutes = await store.dispatch('asyncRouter/generateRoutes', data.roles)
+    accessedRoutes.forEach((item) => {
+      router.addRoute(item)
+    })
     next()
   } else {
-    {
-      let {data} = await store.dispatch('user/GetInfo', token)
-      const accessedRoutes = await store.dispatch('asyncRouter/generateRoutes', data.roles)
-      accessedRoutes.forEach((item) =>{
-        router.addRoute(item)
-      })
-      router.push({ path: to.path })
-    } 
+    next()
   }
-  })
+
+})
